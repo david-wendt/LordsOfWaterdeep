188da20a29758201905005b68657385d3422fc4e
david-wendt <dwendt@stanford.edu>
2024-06-10 23:15:12 -0700

    eval metrics

--- agents/agent.py
+++ agents/agent.py
@@ -6,6 +6,10 @@ class Agent():
     def __init__(self) -> None:
         self.plotQuestsTaken = 0
         self.plotQuestsCompleted = 0
+        self.lordQuestsTaken = 0
+        self.lordQuestsCompleted = 0
+        self.questsTaken = 0
+        self.questsCompleted = 0
         self.buildingsPurchased = 0
         self.intrigueCardsPlayed = 0 
         self.trainMode = False
@@ -15,12 +19,40 @@ class Agent():
     
     def eval(self):
         self.trainMode = False 
+        
+    def getStats(self, ngames):
+        return {
+            'Plot Quests Taken Frac': self.plotQuestsTaken / self.questsTaken,
+            'Plot Quests Completed Frac': self.plotQuestsCompleted / self.questsCompleted,
+            'Lord Quests Taken Frac': self.lordQuestsTaken / self.questsTaken,
+            'Lord Quests Completed Frac': self.lordQuestsCompleted / self.questsCompleted,
+            'Buildings Purchased per Game': self.buildingsPurchased / ngames,
+            'Intrigue Cards Played per Game': self.intrigueCardsPlayed / ngames,
+            'Quests Taken per Game': self.questsTaken / ngames,
+            'Quests Completed per Game': self.questsCompleted / ngames
+        }
+    
+    def isLordQuest(self, quest, player):
+        assert isinstance(quest, Quest)
+        return quest.type in player.lordCard
 
+    def tookQuest(self, action, actions):
+        return (isinstance(action, Quest) and actions[0] != DO_NOT_COMPLETE_QUEST)
+    
     def tookPlotQuest(self, action, actions):
-        return (isinstance(action, Quest) and action.plot and actions[0] != DO_NOT_COMPLETE_QUEST)
+        return self.tookQuest(action, actions) and action.plot
+    
+    def tookLordQuest(self, action, actions, player):
+        return self.tookQuest(action, actions) and self.isLordQuest(action, player)
+    
+    def completedQuest(self, action, actions):
+        return (isinstance(action, Quest) and actions[0] == DO_NOT_COMPLETE_QUEST)
     
     def completedPlotQuest(self, action, actions):
-        return (isinstance(action, Quest) and action.plot and actions[0] == DO_NOT_COMPLETE_QUEST)
+        return self.completedQuest(action, actions) and action.plot
+    
+    def completedLordQuest(self, action, actions, player):
+        return self.completedQuest(action, actions) and self.isLordQuest(action, player)
     
     def purchasedBuilding(self, action):
         return isinstance(action, CustomBuilding) and action.owner is None
@@ -29,18 +61,29 @@ class Agent():
         return isinstance(action, str) and action in INTRIGUES
     
     def actWrapper(self, state, playerState, actions, score):
-        action = self.act(state, playerState, actions, score)
+        action_idx = self.act(state, playerState, actions, score)
+        action = actions[action_idx]
+        # print('actions:', actions)
+        # print('action taken:', )
         if not self.trainMode:
+            if self.tookQuest(action, actions):
+                self.questsTaken += 1
             if self.tookPlotQuest(action, actions):
                 self.plotQuestsTaken += 1
+            if self.tookLordQuest(action, actions, playerState):
+                self.lordQuestsTaken += 1
+            if self.completedQuest(action, actions):
+                self.questsCompleted += 1
             if self.completedPlotQuest(action, actions):
                 self.plotQuestsCompleted += 1
+            if self.completedLordQuest(action, actions, playerState):
+                self.lordQuestsCompleted += 1
             if self.purchasedBuilding(action):
                 self.buildingsPurchased += 1
             if self.playedIntrigueCard(action):
                 self.intrigueCardsPlayed += 1
             
-        return action
+        return action_idx
 
     def act(self, state, playerState, actions, score) -> int:
         ''' Override this in subclasses'''
